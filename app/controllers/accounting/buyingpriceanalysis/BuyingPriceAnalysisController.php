afeaa49a6124241e0cec8656065d28b56c73bc7e
OshenHalgamaYDF <halgamaoshenydf@gmail.com>
2025-09-12 17:56:46 +0530

    Made the table to store the buying price analysis data as well as made the autofill drop down box to the form Aeptember 12th

--- app/controllers/accounting/buyingpriceanalysis/BuyingPriceAnalysisController.php
+++ app/controllers/accounting/buyingpriceanalysis/BuyingPriceAnalysisController.php
@@ -1,36 +1,60 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$database = "ydf-system";
+$username   = "root";
+$password   = "";
+$database   = "ydf-system";
 
-// Connect to database
 $conn = new mysqli($servername, $username, $password, $database);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Get data from form
-$table_name = $_POST['table_name'];
-$col1 = $_POST['col1'];
-$col2 = $_POST['col2'];
-$col3 = $_POST['col3'];
+// Insert when form submitted
+if (isset($_POST['SubmitOffel'])) {
+    $product_code    = $_POST['product_code'];
+    $date            = $_POST['date'];
+    $product_name    = $_POST['product_name'];
+    $scientific_name = $_POST['scientific_name'];
+    $size_range      = $_POST['size_range'];
+    $specification   = $_POST['specification'];
+    $target_price    = $_POST['target_price'];
+    $buyer_name      = $_POST['buyer_name'];
+    $sold_price      = $_POST['sold_price'];
 
-// Build SQL
-$sql = "CREATE TABLE $table_name (
-    $col1,
-    $col2,
-    $col3
-)";
+    $sql = "INSERT INTO buyingpriceanlaysistable
+            (product_code, date, product_name, scientific_name, size_range, specification, target_price, buyer_name, sold_price) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-// Execute query
-if ($conn->query($sql) === TRUE) {
-    echo "Table '$table_name' created successfully!";
-} else {
-    echo "Error creating table: " . $conn->error;
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ssssssdsd", 
+        $product_code, 
+        $date, 
+        $product_name, 
+        $scientific_name, 
+        $size_range, 
+        $specification, 
+        $target_price, 
+        $buyer_name, 
+        $sold_price
+    );
+
+    if ($stmt->execute()) {
+        header("Location: BuyingPriceAnalysis.php");
+        exit();
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+
+    $stmt->close();
 }
 
-$conn->close();
+// Fetch products for dropdown
+$products = [];
+$result = $conn->query("SELECT product_code, product_name, scientific_name FROM products WHERE product_name IS NOT NULL ORDER BY product_name");
+if ($result && $result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $products[] = $row;
+    }
+}
 ?>
--- app/views/accounting/buyingpriceanalysis/BuyingPriceAnalysis.php
+++ app/views/accounting/buyingpriceanalysis/BuyingPriceAnalysis.php
@@ -1,25 +1,86 @@
+<?php 
+include('C:\xampp\htdocs\ydf-system-oshen\app\controllers\accounting\buyingpriceanalysis\BuyingPriceAnalysisController.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>Create Table</title>
-</head>
-<body>
-  <h1>Buying Price Analysis</h1>
-  <form action="create_table.php" method="post">
-    <label>Table Name:</label>
-    <input type="text" name="table_name" required><br><br>
+  <head>
+    <meta charset="UTF-8" />
+    <title>Buying Price Analysis</title>
+  </head>
+  <body>
+    <h1>Buying Price Analysis</h1>
+    <form method="post">
+      <!-- s -->
+      <input type="hidden" name="product_code" required />
 
-    <label>Column 1 (name type):</label>
-    <input type="text" name="col1" placeholder="id INT PRIMARY KEY AUTO_INCREMENT"><br><br>
+      <label>Date:</label>
+      <input type="date" name="date" required />
+      <br /><br />
 
-    <label>Column 2 (name type):</label>
-    <input type="text" name="col2" placeholder="username VARCHAR(50) NOT NULL"><br><br>
+      <label>Product Name:</label>
+      <select name="product_name" required onchange="fillProductDetails()">
+        <option value="">Select Product</option>
+        <?php foreach ($products as $product): ?>
+        <option
+          value="<?php echo htmlspecialchars($product['product_name']); ?>"
+          data-product-code="<?php echo htmlspecialchars($product['product_code']); ?>"
+          data-scientific-name="<?php echo htmlspecialchars($product['scientific_name']); ?>"
+        >
+          <?php echo htmlspecialchars($product['product_name']); ?>
+        </option>
+        <?php endforeach; ?>
+      </select>
+      <br /><br />
 
-    <label>Column 3 (name type):</label>
-    <input type="text" name="col3" placeholder="email VARCHAR(100)"><br><br>
+      <!-- <label>Scientific Name:</label> -->
+      <input type="hidden" name="scientific_name" />
 
-    <input type="submit" value="Create Table">
-  </form>
-</body>
+      <label>Size Range:</label>
+      <input type="text" name="size_range" required />
+      <br /><br />
+
+      <label>Specification:</label>
+      <input type="text" name="specification" required />
+      <br /><br />
+
+      <label>Target Price:</label>
+      <input type="number" step="0.01" name="target_price" required />
+      <br /><br />
+
+      <label>Buyer Name:</label>
+      <input type="text" name="buyer_name" required />
+      <br /><br />
+
+      <label>Sold Price:</label>
+      <input type="number" step="0.01" name="sold_price" required />
+      <br /><br />
+
+      <button
+        type="submit"
+        name="SubmitOffel"
+        class="btn btn-primary px-4"
+        id="modalSubmitBtn"
+      >
+        Add
+      </button>
+    </form>
+    <script>
+      function fillProductDetails() {
+        const productSelect = document.querySelector(
+          'select[name="product_name"]'
+        );
+        const selectedOption =
+          productSelect.options[productSelect.selectedIndex];
+
+        if (selectedOption.dataset.productCode) {
+          document.querySelector('input[name="product_code"]').value =
+            selectedOption.dataset.productCode;
+        }
+        if (selectedOption.dataset.scientificName) {
+          document.querySelector('input[name="scientific_name"]').value =
+            selectedOption.dataset.scientificName;
+        }
+      }
+    </script>
+  </body>
 </html>
