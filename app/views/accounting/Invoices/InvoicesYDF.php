e7030d2f1f82e25d2d66ddc2291cc6aa463c8451
OshenHalgamaYDF <halgamaoshenydf@gmail.com>
2025-10-08 14:11:18 +0530

    First commit wih needed files

---
+++ app/views/accounting/Invoices/InvoicesYDF.php
@@ -0,0 +1,14 @@
+<?php
+include('C:\xampp\htdocs\ydf-system-oshen\app\controllers\accounting\Invoices\InvoicesControllerYDF.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Invoice Generator</title>
+</head>
+<body>
+    
+</body>
+</html>
