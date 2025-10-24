02f6e725e60e3fab9a838614eb78fdbd9e6e89b8
OshenHalgamaYDF <halgamaoshenydf@gmail.com>
2025-10-24 17:04:16 +0530

    Made necessary files

--- app/controllers/accounting/Shipment P&L/USAP&LController.php
+++ app/controllers/accounting/Shipment P&L/USAP&LController.php
@@ -1,2 +1,11 @@
 <?php
+$servername = "localhost";
+$username   = "root";
+$password   = "";
+$database   = "ydf-system";
+
+$conn = new mysqli($servername, $username, $password, $database);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 ?>
